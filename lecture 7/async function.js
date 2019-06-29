8a30338dd8ddf41a9558e3ae0b78225c03b1efe7
prashant kumar <kumarprashant1330@gmail.com>
2019-06-30 02:02:57 +0530

    calculator updated

--- lecture 7/async function.js
+++ lecture 7/async function.js
@@ -1,7 +1,20 @@
- let a= false;
+ /*let a= false;
  setTimeout(function() {
      a= true;
  },1000);
  while(!a){
      console.log(1);
- }
+ }*/
+
+/*const id=setInterval(function (x=50) {
+     
+    if(x==0){
+clearInterval(id)
+
+    }
+    setTimeout(function(){
+        x--;
+    } ,500);
+    console.log(x)
+}, 1000); 
+console.log(new Date())*/
--- practice/calculator.js
+++ practice/calculator.js
@@ -1,2 +1,8 @@
-
+var x;
+function cal(){
+     x= document.forms.show.value;
+    document.forms.show.value=eval(document.forms.show.value)
+   
+}
+document.write(x);
 
