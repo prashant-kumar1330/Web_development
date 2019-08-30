4852144088336a35160dc4057e76594f2baee939
prashant kumar <kumarprashant1330@gmail.com>
2019-08-30 22:41:10 +0530

    react.js

---
+++ react/index.js
@@ -0,0 +1,5 @@
+import React from "react"
+import ReactDom from "react-dom"
+ReactDom.render(<h1><p> hello react</p></h1>,document.getElementById("hello"))
+var newp= document.getElementById("hello");
+newp.innerHTML="hello";
